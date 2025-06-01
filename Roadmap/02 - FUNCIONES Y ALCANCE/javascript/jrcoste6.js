1fd3b1a2b5bec3af475eb39e490bd5a0f35894ee
jrcoste6 <jct702@gmail.com>
2025-06-01 19:49:49 -0400

    Avance del ejercicio #2

--- Roadmap/02 - FUNCIONES Y ALCANCE/javascript/jrcoste6.js
+++ Roadmap/02 - FUNCIONES Y ALCANCE/javascript/jrcoste6.js
@@ -7,5 +7,19 @@ console.log(suma (4,5));
 function resta(a,b) {
     return a - b;
 }
-
 console.log(resta(9,9));
+
+function multiplicacion (a,b){
+    return a*b;
+}
+console.log(multiplicacion(4,5));
+
+function dividir(a,b){
+    return a/b;
+}
+console.log(dividir(6,3));
+
+//Arrow functions o Funciones Flecha
+const elementos = ["aire", "agua", "fuego", "viento"]
+
+console.log(elementos.map((elemento) => (elemento.length)));
